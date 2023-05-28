88e334963dd04ef4cd2b8b605f12cefb794def47
doikentaro <61648865+doiken1130@users.noreply.github.com>
2023-05-28 19:21:54 +0900

    from desktop datetime

--- test.py
+++ test.py
@@ -2,9 +2,12 @@ print("hello!! git!!!!!")
 print("hello!! PyCharm!!!!!")
 import os
 print(os.getcwd())
+from datetime import datetime
 
 print("hello, desktop-OU")
 
 print("from remote changing")
 
 print("certified from notepc-envy")
+
+print(f"{datetime.day}, OK")
